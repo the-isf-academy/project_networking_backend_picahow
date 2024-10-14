6d9a4703b6e4564b9caa29a459e058acf1e6ec0c
Ryan Aw <rakj22@gmail.com>
2024-10-14 11:54:30 +0800

    #today I completed the MVP for models.py which was creating a question and adding the options for the question. #next class I will begin working on the MVP for views.py which is to allow the user to answer the question.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 # models.py
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+
+class Question(Model):
+    question_statement = StringField()
+    option_a = StringField()
+    option_b = StringField()
+    option_c = StringField()
+    option_d = StringField()
+
+    def json_response(self):
+        
+        return {
+            'id': self.id,
+            'question_statement': self.question_statement,
+            'option_a': self.option_a,
+            'option_b': self.option_b,
+            'option_c': self.option_c,
+            'option_d': self.option_d
+        }
+
+
+    
